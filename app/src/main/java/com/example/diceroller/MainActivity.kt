64d879ec07566853dede3a52a62029cf3f3d8d27
ridvanakca <akca.ridvann@gmail.com>
2020-08-18 19:39:15 +0300

    android training

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener{ countUp() }
+    }
+
+
+
+    private fun rollDice() {
+
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+
+       // Toast.makeText(this, "button clicked",
+       //     Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val countUpText: TextView = findViewById(R.id.countUp_text)
+
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                countUpText.text = resultInt.toString()
+            }
+        }
+    }
+}
